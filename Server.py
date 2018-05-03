4b002d767e683485d616c3b098c8ee85f1fdc3f1
Johann Fernandes <johann.fernandes_ug18@ashoka.edu.in>
2018-05-03 21:00:15 +0530

    server update 1.0

--- Server.py
+++ Server.py
@@ -6,7 +6,7 @@ if not os.path.exists(path):
 
 def Main():
     host = '0.0.0.0'
-    port = 2345
+    port = 2222
 
 
     s = socket.socket()
@@ -14,12 +14,21 @@ def Main():
 
     s.listen(1)
     c, addr = s.accept()
-    print "Connection from: " + str(addr)
-    c.send('Please Upload file to be synced.')
+    print ("Connection from: " + str(addr))
+    st = 'Please Upload file to be synced.'
+    byt = st.encode()
+    c.send(byt)
+
     while True:
 
         file_name = c.recv(1024)
+        file_name = str(file_name.decode())
+        print(file_name)
+        #print("1")
         file_data=c.recv(1024)
+        file_data = str(file_data.decode())
+        print(file_data)
+        #file_data.decode()
         if not file_name:
             break
         # print "Request received from client: " + str(data)
